c90738330fa0a100996dc0b82b85ea0946e34a2d
Daniel Steman <daniel-steman@live.nl>
2023-05-02 09:47:58 +0000

    draw two blocks

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,58 @@
-mod azdevops;
-mod utils;
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::{CrosstermBackend, Backend},
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal, Frame
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
-use azdevops::get_repo_list;
+fn ui<B: Backend>(f: &mut Frame<B>) {
+   let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10)
+            ].as_ref()
+        )
+        .split(f.size());
+    let block = Block::default()
+         .title("Block")
+         .borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+    let block = Block::default()
+         .title("Block 2")
+         .borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
+}
 
-#[tokio::main]
-async fn main() {
-    let repos = get_repo_list().await.expect("Failed to get repos");
-    for repo in repos {
-        println!("{}", repo.name)
-    }
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+	terminal.draw(ui)?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
